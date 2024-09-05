d65308b680f9c75ef1f1c44bbcff7553a554a512
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2024-09-05 14:55:33 +0500

    done

---
+++ main.py
@@ -0,0 +1,33 @@
+animal_1_type = input("1-hayvon turini kiriting: ")
+animal_1_weight = input("1-hayvon ogirligini kiriting: ")
+animal_1_age = input("1-hayvon yoshini kiriting: ")
+animal_1_name = input("1-hayvon ismini kiriting: ")
+animal_1_color = input("1-hayvon rangini kiriting: ")
+
+print(f"Hayvon turi: {animal_1_type}, ogirligi: {animal_1_weight}, yoshi: {animal_1_age},  ismi: {animal_1_name}, rangi: {animal_1_color}  ")
+
+animal_2_type = input(f"2-hayvon turini kiriting: ")
+animal_2_weight = input("2-hayvon ogirligini kiriting: ")
+animal_2_age = input("2-hayvon yoshini kiriting: ")
+animal_2_name = input("2-hayvon ismini kiriting: ")
+animal_2_color = input("2-hayvon rangini kiriting: ")
+
+print(f"Hayvon turi: {animal_2_type},  ogirligi: {animal_2_weight},  yoshi: {animal_2_age}, ismi: {animal_2_name}, rangi: {animal_2_color} ")
+
+animal_3_type = input("3-hayvon turini kiriting: ")
+animal_3_weight = input("3-hayvon ogirligini kiriting: ")
+animal_3_age = input("3-hayvon yoshini kiriting: ")
+animal_3_name = input("3-hayvon ismini kiriting: ")
+animal_3_color = input("3-hayvon rangini kiriting: ")
+
+print(f"Hayvon turi: {animal_3_type}, ogirligi: {animal_3_weight}, yoshi: {animal_3_age}, ismi: {animal_3_name}, rangi: {animal_3_color}")
+
+animal_4_type = input("4-hayvon turini kiriting: ")
+animal_4_weight = input("4-hayvon ogirligini kiriting: ")
+animal_4_age = input("4-hayvon yoshini kiriting: ")
+animal_4_name = input("4-hayvon ismini kiriting: ")
+animal_4_color = input("4-hayvon rangini kiriting: ")
+
+print(f"Hayvon turi: {animal_4_type}, ogirligi: {animal_4_weight}, yoshi: {animal_4_age},  ismi: {animal_4_name}, rangi: {animal_4_color}  ")
+
+print(f"Siz hayvonot bogi royxatiga {animal_1_type}, {animal_2_type}, {animal_3_type} va {animal_4_type} haqidagi ma'iumotlarni kirgazdingiz!")
